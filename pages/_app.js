68720580ad58ec1223f894dc78487f4abaf2db35
jvegax <toledo.utrera@hotmail.com>
2022-04-01 14:25:16 +0200

    added apollo client and grapql dependencies

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,30 @@
 import { getLibrary } from "../config/web3";
 import { Web3ReactProvider } from "@web3-react/core";
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
 
 import "../styles/globals.css";
 
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "http://localhost:4000",
+  }),
+});
+
 function MyApp({ Component, pageProps }) {
   return (
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <Component {...pageProps} />
-    </Web3ReactProvider>
+ 
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <ApolloProvider client={client}>
+          <Component {...pageProps} />
+        </ApolloProvider>
+      </Web3ReactProvider>
+
   );
 }
 
--- pages/news.js
+++ pages/news.js
@@ -2,19 +2,35 @@ import React from "react";
 import Layout from "../components/Layout";
 import Article from "../components/Article";
 
+import { gql, useQuery } from "@apollo/client";
+
+const LATEST_NEWS = gql`
+  query {
+    getNews {
+      title
+    }
+  }
+`;
+
 const News = ({ news }) => {
+  try {
+    const { data } = useQuery(LATEST_NEWS);
+    console.log(data.getNews);
+  } catch (error) {
+    console.log(error);
+  }
+  
   return (
     <>
       <Layout page="News" />
 
-      <h1 className="text-xl  text-center text-gray-800 font-bold m-8">The latest news about crypto!</h1>
+      <h1 className="text-xl  text-center text-gray-800 font-bold m-8">
+        The latest news about crypto!
+      </h1>
 
       <div className="grid grid-cols-1 gap-3 text-center m-8 md:grid-cols-3">
         {news.map((article) => (
-          <Article 
-            key={article.id}
-            article={article}
-          />
+          <Article key={article.id} article={article} />
         ))}
       </div>
     </>
@@ -22,7 +38,7 @@ const News = ({ news }) => {
 };
 
 export async function getServerSideProps() {
-  const API_KEY = process.env.API_KEY
+  const API_KEY = process.env.API_KEY;
   const url = `https://min-api.cryptocompare.com/data/v2/news/?lang=EN&api_key=${API_KEY}`;
   const response = await fetch(url);
   const data = await response.json();
